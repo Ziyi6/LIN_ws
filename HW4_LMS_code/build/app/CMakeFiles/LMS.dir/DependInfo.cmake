896595f1c5fb2fcce3f96105386f8447f374bb6c
zhuowei <cheukwaylee@gmail.com>
2022-03-20 16:03:41 +0100

    add HW4 init

---
+++ HW4_LMS_code/build/app/CMakeFiles/LMS.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/code/LIN_ws/HW4_LMS_code/app/main.cpp" "/mnt/hgfs/code/LIN_ws/HW4_LMS_code/build/app/CMakeFiles/LMS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/hgfs/code/LIN_ws/HW4_LMS_code/build/src/CMakeFiles/myfun.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HW4_LMS_code/include/adapt_filt.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+ *
+ * Homework for chapter 4 -- Adaptive filter using LMS & RLS
+ *
+ * Here is the declaration of adapt_filtering function.
+ *
+ **************************************************************************/
+
+#ifndef _ADAPT_FILT_H_
+#define _ADAPT_FILT_H_
+
+// Eigen 核心部分
+#include <eigen3/Eigen/Core>
+// 稠密矩阵的代数运算（逆，特征值等）
+#include <eigen3/Eigen/Dense>
+using namespace Eigen;
+
+const int filter_length = 128;
+static double inputdata[filter_length];
+static Eigen::Matrix<double, 128, 128> P = 100.0 * Eigen::Matrix<double, 128, 128>::Identity();
+
+int adapt_filtering(short input, double *adapt_filter, int filter_length, short *err);
+
+#endif
